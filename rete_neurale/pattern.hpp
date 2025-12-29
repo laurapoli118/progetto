e2dc7c353a913b279197c744a8830bc93fa350a4
LolloVannuc <lorenzovannucci2005@gmail.com>
2025-12-29 21:50:01 +0000

    header di pattern

--- rete_neurale/pattern.hpp
+++ rete_neurale/pattern.hpp
@@ -1,10 +1,36 @@
+#include <vector>   
+#include <string> 
+
+
 class Pattern
 {
 private:
     unsigned height;
     unsigned base;
     unsigned numNeurons;
-    std:vector<int> spins;
+    std::vector<int> neurons;
 
 public:
-}
+    Pattern(int size);
+
+    // Funzioni per settare valori
+
+    void setSpin(unsigned index, int value);
+
+    // Funzioni per ottenere le grandezze del pattern
+
+    int getSpin(unsigned index) const;
+    unsigned getHeight() const;
+    unsigned getBase() const;
+    unsigned getNumNeurons() const;
+    const std::vector<int> &getData() const;
+
+    // Funzione per rumore
+
+    void addNoise(float noisePerc);
+
+    // Gestione e salvataggio pattern
+
+    void saveToFile(const std::string &filename) const;
+    void loadFromFile(const std::string &filename);
+};
