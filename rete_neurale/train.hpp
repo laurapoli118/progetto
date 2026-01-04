dafe715776b2d0c9d085778a592c5a39b5cc8acd
LolloVannuc <lorenzovannucci2005@gmail.com>
2026-01-04 20:13:22 +0000

    vettore double

--- rete_neurale/train.hpp
+++ rete_neurale/train.hpp
@@ -1,20 +1,23 @@
 #include <vector>
-
+#include <pattern.hpp>
 
 class Train
 {
 
 private:
 
-std::vector<float> weights;  // da decidere come la vogliamo fare
+std::vector<std::vector<float>> weights;  // da decidere come la vogliamo fare
 unsigned numNeurons; 
 
 public:
 
+Train(unsigned size);
+
 float getWeight(unsigned i, unsigned j) const;
 
-void setWeight(unsigned i, unsigned j);
+void setWeight(unsigned i, unsigned j, float weight);
 
+void learnPattern(const Pattern& pattern);
 
 
 
