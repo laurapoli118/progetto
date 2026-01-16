78ef09c3451db9e16b35a3d62d4315edab7623bf
laurapoli118 <laura.poli8@studio.unibo.it>
2026-01-16 08:35:48 +0000

    test matrix con + pattern

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -28,3 +28,27 @@ TEST_CASE("Proprietà matrice dei pesi")
     }
 
 }
+
+TEST_CASE("accumulodi due pattern") {
+     const unsigned int size = 4; //misura minore per max velocità
+    Matrix m(size);
+    Pattern p1(size);
+    Pattern p2(size);
+    for(unsigned i=0; i<size; i++) {
+        p1.setNeuron(i, 1.0f); //[1,1,1,1]
+        p2.setNeuron(i, (i<2)? -1.0f : 1.0f); //[-1,-1,1,1]
+    }
+    m.learnPattern(p1);
+    m.learnPattern(p2);
+    SUBCASE("peso si annullano") {
+        CHECK(m.getWeight(0,2)==doctest::Approx(0.0f)); //0.25+(-0.25)
+        CHECK(m.getWeight(1,3)==doctest::Approx(0.0f)); 
+    }
+    SUBCASE("peso si sommano") {
+        CHECK(m.getWeight(2,3)==doctest::Approx(0.5f));
+        CHECK(m.getWeight(0,1)==doctest::Approx(0.5f));
+
+    }
+
+
+}
