d4ddb9999b8f0c05fa9ecec4cc55fc9c243ad3b7
masi <amasini@DESKTOP-E51EDUS.localdomain>
2026-01-21 16:15:15 +0100

    ridimensionamento, path e main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 
     std::cout << "Hopfield Network Creation.\n";
     std::srand(static_cast<unsigned>(std::time(nullptr)));
-    std::cout << "Insert the number of pixels you want to use (50-100): ";
+    std::cout << "Insert the number of pixels you want to use (32~128): ";
     unsigned lato{};
     std::cin >> lato;
     assert(32 <= lato && lato <= 128 && "Image size is too low or too high.");
