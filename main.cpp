8807706cacc756565ddcbdeb3d2e16574a5d825e
masi <amasini@DESKTOP-E51EDUS.localdomain>
2026-01-21 13:01:16 +0100

    chiacchiere varie

--- main.cpp
+++ main.cpp
@@ -7,15 +7,27 @@ int main() {
 
     std::cout << "Hopfield Network Creation.\n";
     std::srand(static_cast<unsigned>(std::time(nullptr)));
-    const unsigned lato = 64;
+    std::cout << "Insert the number of pixels you want to use (50-100): ";
+    unsigned lato{};
+    std::cin >> lato;
+    assert(32 <= lato && lato <= 128 && "Image size is too low or too high.");
     Matrix matrix(lato);
     unsigned int imgsGotten = 0;
     while (true) {
-        std::cout << "[" << imgsGotten + 1 << "] Insert the name of a png file (Mario, Luigi, Toad, Peach or Bowser) or write \'stop\': ";
+        std::cout << "[" << imgsGotten + 1 << "] Insert the name of a png file (Mario, Luigi, Toad, Peach, Bowser or \'all\') or write \'stop\': ";
         std::string imgName;
         std::cin >> imgName;
         if (imgName == "stop") { break; }
         Pattern p(lato);
+        if (imgName == "all") {
+            std::string images[] = {"Mario" , "Luigi", "Bowser", "Toad"};
+            for (int i=0; i<4; i++) {
+                p.loadFromImage(images[i]);
+                matrix.learnPattern(p);
+                imgsGotten++;
+            }
+            break;
+        }
         if (p.loadFromImage(imgName)) { 
             p.display();
             std::cout << "Got it, teaching the network.\n";
