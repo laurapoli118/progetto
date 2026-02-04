9bf081c34f401290ec54d7f9889834e4a6f900ee
Ettore-Tasini <ettore.tasini@studio.unibo.it>
2026-02-04 15:44:39 +0100

    levato commenti

--- test/acquisition_test.cpp
+++ test/acquisition_test.cpp
@@ -14,12 +14,10 @@ namespace fs = std::filesystem;
 
 TEST_CASE("Resize, Compressione Immagine caso base)")
 {
-  // 1. Creiamo un'immagine GRANDE (10x10)
   unsigned bigSize = 10;
   sf::Image bigImage;
   bigImage.create(bigSize, bigSize, sf::Color::Black);
 
-  // 2. Coloriamo l'immagine a "blocchi" (4 quadranti)
   // Quadrante in alto a sinistra: ROSSO
   for (unsigned x = 0; x < bigSize / 2; x++) {
     for (unsigned y = 0; y < bigSize / 2; y++) {
@@ -45,7 +43,6 @@ TEST_CASE("Resize, Compressione Immagine caso base)")
     }
   }
 
-  // 3. Eseguiamo il DOWNSCALING a (2x2)
   unsigned smallSize = 2;
   sf::Image resized  = hp::resize(bigImage, smallSize);
 
@@ -57,38 +54,33 @@ TEST_CASE("Resize, Compressione Immagine caso base)")
 
   SUBCASE("I colori dominanti sono preservati")
   {
-    // Alto-Sinistra deve essere Rosso
     CHECK(resized.getPixel(0, 0) == sf::Color::Red);
 
-    // Alto-Destra deve essere Blu
     CHECK(resized.getPixel(1, 0) == sf::Color::Blue);
 
-    // Basso-Sinistra deve essere Verde
     CHECK(resized.getPixel(0, 1) == sf::Color::Green);
 
-    // Basso-Destra deve essere Giallo
     CHECK(resized.getPixel(1, 1) == sf::Color::Yellow);
   }
 }
 
 TEST_CASE("resize, compressione immagine rettangolare")
 {
-  // 1. Setup: Creiamo immagine rettangolare 20x10
   unsigned orgW = 20;
   unsigned orgH = 10;
   sf::Image rectImg;
-  rectImg.create(orgW, orgH, sf::Color::White); // Sfondo tutto bianco
+  rectImg.create(orgW, orgH, sf::Color::White);
 
   // 2. Inseriamo un pixel rosso
   // Calcoliamo la posizione in modo che finisca esattamente in (2, 2) nel
   // target 5x5. X: 2 * (20 / 5) = 8 Y: 2 * (10 / 5) = 4
   rectImg.setPixel(8, 4, sf::Color::Red);
 
-  // 3. Azione: Ridimensioniamo a 5x5
+  // Ridimensioniamo a 5x5
   unsigned targetSize = 5;
   sf::Image resized   = hp::resize(rectImg, targetSize);
 
-  // 4. Verifiche
+  // Verifiche
   SUBCASE("La dimensione finale deve essere quadrata 5x5")
   {
     CHECK(resized.getSize().x == targetSize);
@@ -97,15 +89,11 @@ TEST_CASE("resize, compressione immagine rettangolare")
 
   SUBCASE("Il pixel rosso deve essere sopravvissuto nella posizione prevista")
   {
-    // Verifichiamo che il pixel in (2, 2) sia diventato Rosso
     CHECK(resized.getPixel(2, 2) == sf::Color::Red);
   }
 
   SUBCASE("Il resto dell'immagine deve essere rimasto bianco")
   {
-    // Controllo paranoico: verifichiamo che un pixel vicino (es. 1,1) sia
-    // ancora bianco Questo conferma che le coordinate X e Y non si sono
-    // mescolate
     CHECK(resized.getPixel(1, 1) == sf::Color::White);
   }
 }
@@ -117,23 +105,20 @@ TEST_CASE("loadFromImage logica")
     fs::create_directories(dirPath);
   }
 
-  hp::Pattern p(2); // Pattern 2x2 (4 neuroni)
+  hp::Pattern p(2);
 
   SUBCASE("Distinzione Bianco/Nero (Binarizzazione)")
   {
     std::string filename = "test_bw";
     sf::Image img;
-    img.create(2, 2, sf::Color::White);   // Sfondo bianco
-    img.setPixel(0, 0, sf::Color::Black); // Un pixel nero in (0,0)
+    img.create(2, 2, sf::Color::White);
+    img.setPixel(0, 0, sf::Color::Black);
 
     img.saveToFile(dirPath + "/" + filename + ".png");
 
     bool success = hp::loadFromImage(filename, p);
     CHECK(success == true);
 
-    // Verifica logica:
-    // Nero -> Luminosità bassa -> Sotto soglia -> 1
-    // Bianco -> Luminosità alta -> Sopra soglia -> -1
     CHECK(p.getNeuron(0) == 1);
     CHECK(p.getNeuron(1) == -1);
     CHECK(p.getNeuron(2) == -1);
@@ -153,10 +138,7 @@ TEST_CASE("loadFromImage logica")
 
     hp::loadFromImage(filename, p);
 
-    // Verifica logica:
-    // Media = 100. Soglia = 0.8 * 100 = 80.
-    // Valore pixel (100) > Soglia (80) -> Considerato Sfondo (-1)
-    for (unsigned i = 0; i < p.getNumNeurons(); ++i) {
+       for (unsigned i = 0; i < p.getNumNeurons(); ++i) {
       CHECK(p.getNeuron(i) == -1);
     }
 
